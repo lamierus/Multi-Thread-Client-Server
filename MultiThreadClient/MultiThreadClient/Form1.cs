ad26c209fc2638067e67f8e91397d1652abd8f1c
Joseph Simon <lamierus@gmail.com>
2016-10-27 19:53:14 -0400

    added files via upload

---
+++ MultiThreadClient/MultiThreadClient/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MultiThreadClient {
+    public partial class Form1 : Form {
+        private TcpClient ClientSocket = new TcpClient() {
+            NoDelay = true
+        };
+        private NetworkStream ServerStream;
+        private Packet ReceivePacket;
+        private Packet SendPacket = new Packet() {
+            ChatName = "Client",
+            ChatDataIdentifier = DataIdentifier.Message
+        };
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            msg("Client Started");
+            ClientSocket.Connect(IPAddress.Loopback.ToString(), 8888);
+            label1.Text = "Client Socket Program - Server Connected ...";
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            try {
+                ServerStream = ClientSocket.GetStream();
+                msg(SendPacket.ChatMessage = "Message from Client!");
+                ServerStream.Write(SendPacket.GetDataStream(), 0, SendPacket.Length);
+                ServerStream.Flush();
+
+                byte[] inStream = new byte[10025];
+                ServerStream.Read(inStream, 0, ClientSocket.ReceiveBufferSize);
+                ReceivePacket = new Packet(inStream);
+                msg("Data from Server: " + ReceivePacket.ChatMessage);
+            } catch (Exception ex) {
+                msg(ex.Message.ToString());
+                msg("Disconnected from server!");
+            }
+        }
+
+        public void msg(string mesg) {
+            textBox1.AppendText(" >> " + mesg);
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.ScrollToCaret();
+        }
+    }
+}
---
+++ MultiThreadClient/MultiThreadClient/Packet.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiThreadClient {
+    // ----------------
+    // Packet Structure
+    // ----------------
+
+    // Description   -> |dataIdentifier|name length|message length|    name   |    message   |
+    // Size in bytes -> |       4      |     4     |       4      |name length|message length|
+
+    public enum DataIdentifier {
+        Message,
+        LogIn,
+        LogOut,
+        Null
+    }
+
+    public class Packet {
+        public DataIdentifier ChatDataIdentifier { get; set; }
+        public string ChatName { get; set; }
+        public string ChatMessage { get; set; }
+
+        public int Length {
+            get { return GetDataStream().Length; }
+        }
+
+        // Default Constructor
+        public Packet() {
+            ChatDataIdentifier = DataIdentifier.Null;
+            ChatMessage = null;
+            ChatName = null;
+        }
+
+        public Packet(byte[] dataStream) {
+            // Read the data identifier from the beginning of the stream (4 bytes)
+            ChatDataIdentifier = (DataIdentifier)BitConverter.ToInt32(dataStream, 0);
+
+            // Read the length of the name (4 bytes)
+            int nameLength = BitConverter.ToInt32(dataStream, 4);
+
+            // Read the length of the message (4 bytes)
+            int msgLength = BitConverter.ToInt32(dataStream, 8);
+
+            // Read the name field
+            if (nameLength > 0)
+                ChatName = Encoding.UTF8.GetString(dataStream, 12, nameLength);
+            else
+                ChatName = null;
+
+            // Read the message field
+            if (msgLength > 0)
+                ChatMessage = Encoding.UTF8.GetString(dataStream, 12 + nameLength, msgLength);
+            else
+                ChatMessage = null;
+        }
+
+        // Converts the packet into a byte array for sending/receiving 
+        public byte[] GetDataStream() {
+            List<byte> dataStream = new List<byte>();
+
+            // Add the dataIdentifier
+            dataStream.AddRange(BitConverter.GetBytes((int)ChatDataIdentifier));
+
+            // Add the name length
+            if (ChatName != null)
+                dataStream.AddRange(BitConverter.GetBytes(ChatName.Length));
+            else
+                dataStream.AddRange(BitConverter.GetBytes(0));
+
+            // Add the message length
+            if (ChatMessage != null)
+                dataStream.AddRange(BitConverter.GetBytes(ChatMessage.Length));
+            else
+                dataStream.AddRange(BitConverter.GetBytes(0));
+
+            // Add the name
+            if (ChatName != null)
+                dataStream.AddRange(Encoding.UTF8.GetBytes(ChatName));
+
+            // Add the message
+            if (ChatMessage != null)
+                dataStream.AddRange(Encoding.UTF8.GetBytes(ChatMessage));
+
+            return dataStream.ToArray();
+        }
+    }
+}
