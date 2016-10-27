da66a9cdfa9f60e9b9df824984485ee47d9b1f08
J S <lamierus@gmail.com>
2016-10-27 19:48:34 -0400

    commit of the rest of the repository

---
+++ MultiThreadServer/MultiThreadServer/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadServer {
+    //Class to handle each client request seperately
+    public class Client {
+        public TcpClient ClientSocket;
+        public int ClientNo { get; set; }
+
+        public Packet ReceivePack;
+        public Packet SendPack = new Packet(){
+            ChatName = "Server",
+            ChatDataIdentifier = DataIdentifier.Message
+        };
+
+        public void startClient(TcpClient inClientSocket, int clineNo) {
+            ClientSocket = inClientSocket;
+            ClientSocket.NoDelay = true;
+            ClientNo = clineNo;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        private void doChat() {
+            int requestCount = 0;
+            byte[] inStream = new byte[10025];
+            NetworkStream networkStream;
+
+            while ((true)) {
+                try {
+                    requestCount++;
+                    networkStream = ClientSocket.GetStream();
+                    networkStream.Read(inStream, 0, ClientSocket.ReceiveBufferSize);
+                    ReceivePack = new Packet(inStream);
+                    Console.WriteLine(" >> " + "From client - " + ReceivePack.ChatName + ClientNo.ToString() + ": " + ReceivePack.ChatMessage);
+                    Program.Broadcast(ReceivePack, true);
+
+                    SendPack.ChatMessage = "Response to client(" + ReceivePack.ChatName + "): " + requestCount.ToString();
+                    networkStream.Write(SendPack.GetDataStream(), 0, SendPack.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + SendPack.ChatMessage);
+                } catch (Exception ex) {
+                    Console.WriteLine(" >> " + ex.Message.ToString());
+                    Console.WriteLine(" >> Client " + ClientNo + " Disconnected");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ MultiThreadServer/MultiThreadServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiThreadServer {
+    class Program {
+        private static int Counter = 0;
+        public static List<Client> ClientList = new List<Client>();
+
+        static void Main(string[] args) {
+            TcpListener serverSocket = new TcpListener(IPAddress.Any, 8888);
+
+            serverSocket.Start();
+            Console.WriteLine(" >> " + "Server Started");
+            
+            Thread AcceptClients = new Thread(ConnectClient);
+            AcceptClients.Start(serverSocket);
+
+            string input = string.Empty;
+
+            while (input != "/quit") {
+                input = Console.ReadLine();
+            }
+            
+            AcceptClients.Interrupt();
+            serverSocket.Stop();
+            Console.WriteLine(" >> exit");
+            Console.ReadLine();
+        }
+
+        private static void ConnectClient(object parameter) {
+            var serverSocket = parameter as TcpListener;
+            var clientSocket = default(TcpClient);
+            while (true) {
+                try {
+                    clientSocket = serverSocket.AcceptTcpClient();
+                    Console.WriteLine(" >> " + "Client No:" + (Counter + 1).ToString() + " started!");
+                    ClientList.Add(new Client());
+                    ClientList[Counter++].startClient(clientSocket, Counter);
+                } catch (SocketException e) {
+                    clientSocket.Close();
+                    break;
+                }
+            }
+        }
+
+        public static void Broadcast(Packet packet, bool flag) {
+            foreach (Client client in ClientList) {
+                //NetworkStream broadcastStream = client.ClientSocket.GetStream();
+
+                client.ClientSocket.GetStream().Write(packet.GetDataStream(), 0, packet.Length);
+                client.ClientSocket.GetStream().Flush();
+            }
+        }
+    }
+}
